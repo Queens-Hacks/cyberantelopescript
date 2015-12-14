e9f91f975786c2e5f35ecc5cf06a460f814b0c54
Jake Pittis <jakepittis@gmail.com>
2015-12-14 00:54:29 -0500

    added mountain range

--- src/cas/world.cljs
+++ src/cas/world.cljs
@@ -26,3 +26,36 @@
                   (< level grass-to-sky) :grass
                   :else :sky)))]
     (vec (map #(new-row x (kind %)) (range y)))))
+
+(defn rand-in-range
+  [lower upper]
+  (+ (rand-int (- upper lower)) lower))
+
+(defn limit-range
+  [value lower upper]
+  (cond (< value lower) lower
+        (> value upper) upper
+        :else value))
+
+(defn new-mountain-range
+  [length lower upper]
+  (loop [last-level (rand-in-range lower upper)
+         result [last-level]]
+    (if (= (count result) length)
+      result
+      (let [next (let [up-or-down (- (rand-int 3) 1)]
+                     (limit-range (+ last-level up-or-down)
+                                  lower
+                                  upper))]
+        (recur next (conj result next))))))
+
+(defn new-mountain-world
+  [width height lower upper]
+  (let [mrange (new-mountain-range width lower upper)]
+    (letfn [(new-column [bound]
+              (vec (concat (repeat bound
+                              (tile :stone))
+                      (repeat (- height bound)
+                              (tile :sky)))))]
+    (vec (map new-column mrange)))))
+
